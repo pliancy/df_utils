7695a9ebe90a87d7bbc763da24886ffef351a19e
Zach Brak <zach.brak@pliancy.com>
2023-06-20 16:00:52 -0400

    simplifying build to only package index

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
-const df_utils = require("./includes/df_utils.js");
-// const file_two = require("./includes/file_two");
+//  It is more performant to deduplicate using `array_agg` with a limit
+//  clause in BigQuery:
+//  https://github.com/dbt-labs/dbt-utils/issues/335#issuecomment-788157572
+function deduplicate(relation, partition_by, order_by, incremental) {
+  const sql = `
+    select unique.*
+    from (
+      select
+        array_agg(original
+        order by ${order_by}
+        limit 1)[offset(0)] as unique
+      from ${relation} original
+      ${incremental}
+      group by ${partition_by}
+    )
+  `;
+  return sql;
+}
 
-module.exports = (params) => {
 
-    params = {
-      ...params
-    };
-
-    // Publish and return datasets.
-    let result = {
-    df_utils: df_utils(params),
-    //   file_two: file_two(params)
-    };
-
-    return result;
+module.exports = {
+  deduplicate
+  // group_by,
+  // safe_add,
+  // safe_divide,
+  // safe_subtract,
+  // star
 }
