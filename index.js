dc431475098d7e5d307a13de26ef9d08df690a7e
Zach Brak <zach.brak@pliancy.com>
2023-06-20 15:08:19 -0400

    updated index

--- index.js
+++ index.js
@@ -1,19 +1,15 @@
-// TODO: import files from includes
-// const file_one = require("./includes/file_one");
+const df_utils = require("./includes/df_utils");
 // const file_two = require("./includes/file_two");
 
 module.exports = (params) => {
 
     params = {
-      // TODO: set default params
-
       ...params
     };
 
     // Publish and return datasets.
     let result = {
-    // TODO: update files to call with params
-    //   file_one: file_one(params),
+    df_utils: df_utils(params),
     //   file_two: file_two(params)
     };
 
