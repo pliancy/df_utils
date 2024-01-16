47e204e2741c3b3ecadf2355824aa4021726a0be
Zach Brak <zach.brak@pliancy.com>
2024-01-16 17:00:56 -0500

    correct package index

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+// Description: Index file for the library
+const group_by = require('./includes/group_by');
+const unpivot = require('./includes/unpivot');
+
+
 module.exports = {
   deduplicate,
   group_by,
