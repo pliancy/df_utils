9e596b8803c251b97c25dfd7ed0696598866aa46
Justin <justin.mcmahon@pliancy.comrything.com>
2024-01-16 15:38:38 -0800

    make the tests more uniform

--- includes/deduplicate.spec.js
+++ includes/deduplicate.spec.js
@@ -3,6 +3,17 @@ const { deduplicate } = require('../')
 describe('deduplicate', () => {
     it('works better as an exported function than as an object', () => {
         const deduped = deduplicate('relation', 'partition_by', 'order_by', 'incremental')
-        expect(deduped).toBeTruthy()
+        expect(deduped).toEqual(`
+    select unique.*
+    from (
+      select
+        array_agg(original
+        order by order_by
+        limit 1)[offset(0)] as unique
+      from relation original
+      incremental
+      group by partition_by
+    )
+  `)
     })
 })
--- includes/group_by.spec.js
+++ includes/group_by.spec.js
@@ -2,6 +2,7 @@ const { group_by } = require('../')
 
 describe('group_by', () => {
     it('is awkward to import this one', () => {
-        expect(group_by.group_by(0, [])).toEqual('group by ')
+        const groupBy = group_by.group_by(0, [])
+        expect(groupBy).toEqual('group by ')
     })
 })
