e87efc280b8af9de72d44bef5e18bf3bae448341
Zach Brak <zach.brak@pliancy.com>
2024-01-16 17:21:28 -0500

    Add deduplicate function and fix formatting in index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Description: Index file for the library
+const deduplicate = require('./includes/deduplicate');
 const group_by = require('./includes/group_by');
 const unpivot = require('./includes/unpivot');
-
 
 module.exports = {
   deduplicate,
