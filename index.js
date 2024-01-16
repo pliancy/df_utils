140b465a68586fc5a68c7676ba6fcbdbe9035bae
Zach Brak <zach.brak@pliancy.com>
2024-01-16 17:58:50 -0500

    Add deduplication, grouping, and unpivoting functions

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const deduplicate = require('./includes/deduplicate');
 const group_by = require('./includes/group_by');
 const unpivot = require('./includes/unpivot');
 
+deduplicate.deduplicate();
+group_by.group_by();
+unpivot.unpivot();
+
 module.exports = {
   deduplicate,
   group_by,
