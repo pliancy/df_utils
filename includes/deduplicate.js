ea099a4ef5c463dc1b9370c0df08dfd21ed0c7f7
Zach Brak <zach.brak@pliancy.com>
2024-01-17 13:37:51 -0500

    Documentation, refactor unpivot function

--- includes/deduplicate.js
+++ includes/deduplicate.js
@@ -1,8 +1,18 @@
+/**
+ * Deduplicates records in a relation based on specified partition and order criteria.
+ * It generates a SQL query that uses `array_agg` with a limit clause in BigQuery.
+ *
+ * @param {string} relation - The name of the relation to deduplicate.
+ * @param {string} partition_by - The column(s) to partition the deduplication on.
+ * @param {string} order_by - The column(s) to order the records within each partition.
+ * @param {string} incremental - Optional incremental clause to include in the query.
+ * @returns {string} The SQL query for deduplicating the relation.
+ */
 //  It is more performant to deduplicate using `array_agg` with a limit
 //  clause in BigQuery:
 //  https://github.com/dbt-labs/dbt-utils/issues/335#issuecomment-788157572
 function deduplicate(relation, partition_by, order_by, incremental) {
-    const sql = `
+  const sql = `
     select unique.*
     from (
       select
@@ -14,10 +24,10 @@ function deduplicate(relation, partition_by, order_by, incremental) {
       group by ${partition_by}
     )
   `;
-    return sql;
+  return sql;
 }
 
 
 module.exports = {
-    deduplicate
+  deduplicate
 }
--- includes/unpivot.js
+++ includes/unpivot.js
@@ -1,32 +1,37 @@
-function unpivot(relation, cast_to = 'varchar', exclude = [], remove = [], field_name = 'field_name', value_name = 'value') {
+/**
+ * Unpivots a given relation by transforming columns into rows.
+ * This function generates a SQL query that performs the unpivot operation.
+ *
+ * @param {string} relation - The name of the relation to unpivot.
+ * @param {string} [cast_to='varchar'] - The data type to cast the value column to.
+ * @param {string[]} columns= [] - An array representing the collection of columns in the table.
+ * @param {string[]} [exclude=[]] - An array of column names to exclude from the unpivot operation.
+ * @param {string[]} [remove=[]] - An array of column names to remove from the unpivot operation.
+ * @param {string} [field_name='field_name'] - The name of the column that will store the original column names.
+ * @param {string} [value_name='value'] - The name of the column that will store the values.
+ * @returns {string} - The generated SQL query for the unpivot operation.
+ */
+function unpivot(relation, cast_to = 'string', columns = [], exclude = [], remove = [], field_name = 'field_name', value_name = 'value') {
     if (!relation) {
         throw new Error("Error: argument 'relation' is required for 'unpivot' function.");
     }
 
     const include_cols = [];
-    const table_columns = {};
-    table_columns[relation] = [];
-
-    // Check if relation is valid and not ephemeral
-    dbt_utils._is_relation(relation, 'unpivot');
-    dbt_utils._is_ephemeral(relation, 'unpivot');
-
-    const cols = adapter.get_columns_in_relation(relation);
+    const cols = columns;
 
     for (const col of cols) {
-        if (!remove.map(col => col.toLowerCase()).includes(col.column.toLowerCase()) && !exclude.map(col => col.toLowerCase()).includes(col.column.toLowerCase())) {
+        if (!remove.includes(col.toLowerCase()) && !exclude.includes(col.toLowerCase())) {
             include_cols.push(col);
         }
     }
-
     let sql = '';
     for (let i = 0; i < include_cols.length; i++) {
         const col = include_cols[i];
         sql += `
         select
-          ${exclude.join(', ')},
-          cast('${col.column}' as ${dbt.type_string()}) as ${field_name},
-          cast(${col.data_type === 'boolean' ? dbt.cast_bool_to_text(col.column) : col.column} as ${cast_to}) as ${value_name}
+          ${exclude.toString()},
+          cast('${col}' as string) as ${field_name},
+          cast(${col} as ${cast_to}) as ${value_name}
         from ${relation}
       `;
         if (i !== include_cols.length - 1) {
